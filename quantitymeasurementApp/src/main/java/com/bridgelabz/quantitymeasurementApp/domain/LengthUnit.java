eb7fb26aed43cb4e1279fd9ebd446f84518778e7
Ashu398 <chaubeyashutosh980@gmail.com>
2026-05-11 14:41:44 +0530

    [Ashutosh Chaubey]Feature: UC4 - Extend LengthUnit to support Yard and Mile

--- quantitymeasurementApp/src/main/java/com/bridgelabz/quantitymeasurementApp/domain/LengthUnit.java
+++ quantitymeasurementApp/src/main/java/com/bridgelabz/quantitymeasurementApp/domain/LengthUnit.java
@@ -2,7 +2,9 @@ package com.bridgelabz.quantitymeasurementApp.domain;
 
 public enum LengthUnit implements Unit {
     FEET(12.0), // 1 Foot = 12.0 base units (Inches)
-    INCHES(1.0);
+    INCHES(1.0),
+    YARD(36.0),    // 1 Yard = 3 Feet = 36 Inches
+    MILE(63360.0); // 1 Mile = 1760 Yards = 5280 Feet = 63360 Inches
     private final double baseConversionFactor;
 
     LengthUnit(double baseConversionFactor) {
--- quantitymeasurementApp/src/test/java/com/bridgelabz/quantitymeasurementApp/domain/QuantityTest.java
+++ quantitymeasurementApp/src/test/java/com/bridgelabz/quantitymeasurementApp/domain/QuantityTest.java
@@ -62,4 +62,47 @@ public class QuantityTest {
         Quantity inch = new Quantity(1.0, LengthUnit.INCHES);
         assertNotEquals(foot, inch); // Proves 1 ft != 1 in
     }
+    // --- UC4 Tests (Extended Length Units) ---
+
+    @Test
+    void given3FeetAnd1Yard_ShouldReturnEqual() {
+        Quantity feet = new Quantity(3.0, LengthUnit.FEET);
+        Quantity yard = new Quantity(1.0, LengthUnit.YARD);
+        assertEquals(feet, yard); // Proves 3 ft == 1 yd
+    }
+
+    @Test
+    void given1YardAnd3Feet_ShouldReturnEqual() {
+        Quantity yard = new Quantity(1.0, LengthUnit.YARD);
+        Quantity feet = new Quantity(3.0, LengthUnit.FEET);
+        assertEquals(yard, feet);
+    }
+
+    @Test
+    void given1YardAnd36Inches_ShouldReturnEqual() {
+        Quantity yard = new Quantity(1.0, LengthUnit.YARD);
+        Quantity inch = new Quantity(36.0, LengthUnit.INCHES);
+        assertEquals(yard, inch); // Proves 1 yd == 36 in
+    }
+
+    @Test
+    void given36InchesAnd1Yard_ShouldReturnEqual() {
+        Quantity inch = new Quantity(36.0, LengthUnit.INCHES);
+        Quantity yard = new Quantity(1.0, LengthUnit.YARD);
+        assertEquals(inch, yard);
+    }
+
+    @Test
+    void given1MileAnd1760Yards_ShouldReturnEqual() {
+        Quantity mile = new Quantity(1.0, LengthUnit.MILE);
+        Quantity yard = new Quantity(1760.0, LengthUnit.YARD);
+        assertEquals(mile, yard); // Proves 1 mile == 1760 yds
+    }
+
+    @Test
+    void given1YardAnd1Mile_ShouldReturnNotEqual() {
+        Quantity yard = new Quantity(1.0, LengthUnit.YARD);
+        Quantity mile = new Quantity(1.0, LengthUnit.MILE);
+        assertNotEquals(yard, mile);
+    }
 }
